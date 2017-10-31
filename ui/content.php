c128f4636d8d8de22ca73d24ee13cfe45b31f3f1
pete-rai <pete@rai.org.uk>
2017-10-31 20:27:42 +0000

    Better content list layout

--- ui/content.php
+++ ui/content.php
@@ -3,14 +3,42 @@
   include_once 'lib/helper.php';
   include_once 'lib/model.php';
 
-  $content = Model::getAllContent ();
+  define ('CONTENT_COLS', 3);
 
-  foreach ($content as &$movie)
+  $content = Model::getAllContent ();
+  $years   = [];
+  $words   = ['president', 'nuclear', ''];
+
+  foreach ($content as $movie)
   {
-      $movie = "$movie->year - <a href='bubbles.php?topic=$movie->content_id'>$movie->title</a>";
+      if (!isset ($years [$movie->year]))
+      {
+          $years [$movie->year] = [];
+      }
+
+      $years [$movie->year][] = "<td><a href='bubbles.php?topic=$movie->content_id'>$movie->title</a></td>";
   }
 
-  $list = '<ul><li>'.implode ('</li><li>', $content).'</li></ul>';
+  $rows = [];
+
+  foreach ($years as $year => $movies)
+  {
+      $rows [] = "<tr><th colspan='".CONTENT_COLS."'>$year</th></tr>";
+
+      for ($idx = 0 ; $idx < count ($movies) ; $idx += CONTENT_COLS)
+      {
+          $row = '';
+
+          for ($n = 0 ; $n < CONTENT_COLS ; $n++)
+          {
+              $row .= isset ($movies [$idx + $n]) ? $movies [$idx + $n] : '<td></td>';
+          }
+
+          $rows [] = "<tr>$row</tr>";
+      }
+  }
+
+  $table = '<table>'.implode ('', $rows).'</table>';
 
 ?>
 <!DOCTYPE html>
@@ -18,8 +46,41 @@
   <head>
     <title>wooc &raquo; all content</title>
     <meta charset='UTF-8'>
+    <style>
+
+    html
+    {
+        font-family: Calibri, Candara, Segoe, Optima, Arial, sans-serif;
+    }
+
+    table
+    {
+        border: 1px solid darkgrey;
+        border-collapse: collapse;
+        margin: 5px;
+    }
+
+    td, th
+    {
+        border: 1px solid darkgrey;
+        padding: 5px;
+        text-align: center;
+    }
+
+    th
+    {
+        background-color: lightgrey;
+    }
+    </style>
   </head>
   <body>
-    <?php echo $list; ?>
+    <h1>Word of our Culture</h1>
+    <form action='bubbles.php'>
+        <input id='topic' name='topic' placeholder="word" />
+        <input type='submit' value='go for it'/>
+    </form>
+
+
+    <?php echo $table; ?>
   </body>
 </html>
