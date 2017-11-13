562011573968386679276bbffec887d0a9d006dc
pete-rai <pete@rai.org.uk>
2017-11-13 23:47:47 +0000

    Improvements to bubbles html layout

--- ui/bubbles.php
+++ ui/bubbles.php
@@ -28,48 +28,37 @@
 
 ?>
 <!DOCTYPE html>
-<html lang='en'>
+<html lang="en">
   <head>
     <title><?php echo $title; ?></title>
-    <meta charset='UTF-8'>
-    <link rel='stylesheet' href='css/jquery.slidein.css'>
-    <link rel='stylesheet' href='css/bubbles.css'>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.8/p5.min.js'></script>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
-    <script src='//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>
-    <script src='js/jquery.slidein.js'></script>
-    <script src='js/bubbles.p5.js'></script>
-    <script src="//platform-api.sharethis.com/js/sharethis.js#property=59ed1844899d1d001141cf42&product=unknown" async='async'></script>
+    <meta charset="UTF-8">
+    <meta name=viewport content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/jquery.slidein.css">
+    <link rel="stylesheet" href="css/bubbles.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.8/p5.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+    <script src="js/whenpresent.js"></script>
+    <script src="js/jquery.slidein.js"></script>
+    <script src="js/bubbles.p5.js"></script>
+    <script src="//platform-api.sharethis.com/js/sharethis.js#property=59ed1844899d1d001141cf42&product=unknown" async="async"></script>
     <script>
 
-      function whenPresent (control, action)
+      whenPresent (".st-sticky-share-buttons", function (control)
       {
-          if ($(control).length === 0)
-          {
-              setTimeout (whenPresent.bind (this, control, action), 500);
-          }
-          else
-          {
-              action ($(control));
-          }
-      }
-
-      whenPresent ('.st-sticky-share-buttons', function (control)
-      {
-          control.css ('opacity', '0.7');
+          control.css ("opacity", "0.7");
       });
 
-      var source   = '<?php echo $source; ?>';
+      var source   = "<?php echo $source; ?>";
       var onselect = function (key, data)
       {
-          $('#next').attr ('action', 'bubbles.php?topic=' + data.topic);
-          $('input#topic').val (key);
-          $('#next').submit ();
+          $("#next").attr ("action", "bubbles.php?topic=" + data.topic);
+          $("input#topic").val (key);
+          $("#next").submit ();
       };
 
       var ontip = function (key, data)
       {
-          console.log (data);
           return "double click to open the '" + data.name + "' bubble";
       };
 
@@ -77,23 +66,23 @@
 
       $(document).ready (function()
       {
-          $('#steps').slidein ({ open: false, opacity: 0.7, peek: 0, breadth: 300, toOpen: 'hover', toClose: 'hover', prompt: 'info' });
+          $("#steps").slidein ({ open: false, opacity: 0.7, peek: 0, breadth: 300, toOpen: "hover", toClose: "hover", prompt: "info" });
       });
 
     </script>
   </head>
   <body>
-    <form id='next' action='' method='post'>
+    <form id="next" action="#" method="post">
       <?php echo $fields; ?>
     </form>
     <h1><?php echo "$type: $subject"; ?></h1>
-    <div id='steps'>
-        <h2>Words of our Culture</h2>
+    <div id="steps">
+      <h2>Words of our Culture</h2>
       <ol>
         <?php echo $steps; ?>
       </ol>
-      <span id='footer'>
-        <a href='https://github.com/pete-rai/words-of-our-culture#words-of-our-culture'>About</a>&nbsp;|&nbsp;<a href='content.php'>Index</a>
+      <span id="footer">
+        <a href="https://github.com/pete-rai/words-of-our-culture#words-of-our-culture">About</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="content.php">Index</a>
       </span>
     </div>
   </body>
