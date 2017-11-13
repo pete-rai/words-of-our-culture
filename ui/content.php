895856e01d4dc7269d213069f66c33fabe2dd2bf
pete-rai <pete@rai.org.uk>
2017-11-13 23:46:59 +0000

    Improvements to content index page

--- ui/content.php
+++ ui/content.php
@@ -5,6 +5,11 @@
 
   define ('CONTENT_COLS', 3);
 
+  function item ($href, $text)
+  {
+      return "<div class='item'><a href='$href'>$text</a></div>";
+  }
+
   $content = Model::getAllContent ();
   $years   = [];
 
@@ -15,10 +20,18 @@
           $years [$movie->year] = [];
       }
 
-      $years [$movie->year][] = "<div class='item'><a href='bubbles.php?topic=$movie->content_id'>$movie->title</a></div>";
+      $years [$movie->year][] = item ("bubbles.php?topic=$movie->content_id", $movie->title);
   }
 
+  $randMovie = item ('random.php?type=movie', 'Random Movie');
+  $randWord  = item ('random.php?type=word' , 'Random Word' );
+
   $rows = [];
+
+  $rows [] = "<h2>Random</h2>";
+  $rows [] = "<div class='container'>";
+  $rows [] = "<div class='col-md-6'>$randMovie</div><div class='col-md-6'>$randWord</div>";
+  $rows [] = "</div>";
 
   foreach ($years as $year => $movies)
   {
@@ -43,36 +56,26 @@
       $rows [] = "</div>";
   }
 
-  $table = implode ('', $rows);
+  $table = implode ("\n", $rows);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>wooc &raquo; all content</title>
+  <title>wooc &raquo; Content Index</title>
   <meta charset="UTF-8">
   <meta name=viewport content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" />
-  <style>
-
-    html { font-family: Calibri, Candara, Segoe, Optima, Arial, sans-serif;}
-    body { background-color: #dcdcdc; margin-bottom: 15px;}
-    div.item { padding: 8px; margin: 8px; font-size: 1.5em; text-align: center; background-color: white; border-radius: 10px; }
-    h1 { text-align: center; color: #000080;}
-    h2 { background-color: #000080; color: #dcdcdc; text-align: center; padding: 6px; }
-    form { text-align: center; }
-    input { color: #000080; border: 1px solid grey; text-align: center; font-size: 1.5em; margin: 10px; margin-top: 15px; width: 70%; }
-
-  </style>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  <script src="//platform-api.sharethis.com/js/sharethis.js#property=59ed1844899d1d001141cf42&product=unknown" async='async'></script>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" />
+  <link rel="stylesheet" href="css/content.css">
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="//platform-api.sharethis.com/js/sharethis.js#property=59ed1844899d1d001141cf42&product=unknown" async="async"></script>
 </head>
 <body>
   <h1>Words of our Culture</h1>
   <h2>Words</h2>
-  <form action='bubbles.php'>
-    <input id='topic' name='topic' placeholder="type a word and hit return" />
+  <form action="bubbles.php">
+    <input id="topic" name="topic" placeholder="type word + hit return" />
   </form>
   <?php echo $table; ?>
 </body>
